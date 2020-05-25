f43018b396291841ac0a6f78214187e0f072e179
Max Schmitt <max@schmitt.mx>
2020-05-26 01:01:12 +0200

    feat: landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,17 @@
 import Nav from '../components/nav'
 import MainLayout from '../layouts/main'
+import { Meta } from 'types/blog'
+import Link from 'next/link'
 
-export default function IndexPage() {
+interface IndexProps {
+  posts: (Meta & {
+    slug: string
+  })[]
+}
+
+const Index: React.FunctionComponent<IndexProps> = ({
+  posts
+}) => {
   return (
     <MainLayout>
       <>
@@ -11,51 +21,56 @@ export default function IndexPage() {
               Playwright Community
             </span>
             <p className="text-lg text-gray-600">
-              A central home for tutorials, tooling and projects in the Playwright ecosystem.
+              A central home for tutorials, tooling, and showcases of the Playwright ecosystem.
             </p>
           </div>
         </header>
 
         <div className="container mx-auto flex flex-wrap py-2">
-          <section className="w-full flex flex-col items-center px-3">
-            <article className="flex flex-col shadow my-4">
-              <div className="bg-white flex flex-col justify-start p-6">
-                <a href="#" className="text-blue-700 text-sm font-bold uppercase pb-4">Technology</a>
-                <a href="#" className="text-3xl font-bold hover:text-gray-700 pb-4">Lorem Ipsum Dolor Sit Amet Dolor Sit Amet</a>
-                <a href="#" className="text-sm pb-3">
-                  By <a href="#" className="font-semibold hover:text-gray-800">David Grzyb</a>, Published on April 25th, 2020
-                    </a>
-                <a href="#" className="pb-6">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus quis porta dui. Ut eu iaculis massa. Sed ornare ligula lacus, quis iaculis dui porta volutpat. In sit amet posuere magna..</a>
-                <a href="#" className="uppercase text-gray-800 hover:text-black">Continue Reading <i className="fas fa-arrow-right"></i></a>
-              </div>
-            </article>
+          <section className="w-full flex flex-col px-3">
 
-            <article className="flex flex-col shadow my-4">
-              <div className="bg-white flex flex-col justify-start p-6">
-                <a href="#" className="text-blue-700 text-sm font-bold uppercase pb-4">Automotive, Finance</a>
-                <a href="#" className="text-3xl font-bold hover:text-gray-700 pb-4">Lorem Ipsum Dolor Sit Amet Dolor Sit Amet</a>
-                <a href="#" className="text-sm pb-3">
-                  By <a href="#" className="font-semibold hover:text-gray-800">David Grzyb</a>, Published on January 12th, 2020
-                    </a>
-                <a href="#" className="pb-6">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus quis porta dui. Ut eu iaculis massa. Sed ornare ligula lacus, quis iaculis dui porta volutpat. In sit amet posuere magna..</a>
-                <a href="#" className="uppercase text-gray-800 hover:text-black">Continue Reading <i className="fas fa-arrow-right"></i></a>
-              </div>
-            </article>
+            {posts.map(post => <article className="flex shadow my-4">
+              <Link href={`blog/${post.slug}`}>
+                <a>
+                  <div className="bg-white flex flex-col justify-start p-6">
+                    <span className="text-blue-700 text-sm font-bold uppercase pb-4">{post.tags.join(", ")}</span>
+                    <span className="text-3xl font-bold hover:text-gray-700 pb-4">{post.title}</span>
+                    <span className="text-sm pb-3">
+                      By <span className="font-semibold hover:text-gray-800">{post.author}</span>, Published on {new Date(post.date).toLocaleDateString()}
+                    </span>
+                    <span className="pb-6">{post.description}</span>
+                    <span className="uppercase text-gray-800 hover:text-black">Continue Reading <i className="fas fa-arrow-right"></i></span>
+                  </div>
+                </a>
+              </Link>
+            </article>)}
 
-            <article className="flex flex-col shadow my-4">
-              <div className="bg-white flex flex-col justify-start p-6">
-                <a href="#" className="text-blue-700 text-sm font-bold uppercase pb-4">Sports</a>
-                <a href="#" className="text-3xl font-bold hover:text-gray-700 pb-4">Lorem Ipsum Dolor Sit Amet Dolor Sit Amet</a>
-                <a href="#" className="text-sm pb-3">
-                  By <a href="#" className="font-semibold hover:text-gray-800">David Grzyb</a>, Published on October 22nd, 2019
-                    </a>
-                <a href="#" className="pb-6">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus quis porta dui. Ut eu iaculis massa. Sed ornare ligula lacus, quis iaculis dui porta volutpat. In sit amet posuere magna..</a>
-                <a href="#" className="uppercase text-gray-800 hover:text-black">Continue Reading <i className="fas fa-arrow-right"></i></a>
-              </div>
-            </article>
           </section>
         </div>
       </>
     </MainLayout>
   )
 }
+
+
+Index.getInitialProps = async () => {
+  const posts = (context => {
+    const keys = context.keys();
+    const values = keys.map(context);
+    return keys.map((key, index) => {
+      // Create slug from filename
+      const slug = key.match(/([\w-]+)(?:\/index|)\.mdx$/)[1]
+      const { meta }: any = values[index]
+      return {
+        ...meta,
+        slug,
+        date: new Date(meta.date)
+      };
+    });
+  })(require.context("./blog", true, /\.mdx$/));
+  return {
+    posts: posts.sort((a, b) => b.date - a.date)
+  }
+}
+
+export default Index
---
+++ types/blog.d.ts
@@ -0,0 +1,7 @@
+export interface Meta {
+  title: string
+  description: string
+  author: string
+  date: Date
+  tags: string[]
+}
