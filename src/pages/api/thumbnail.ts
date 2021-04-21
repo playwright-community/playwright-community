164f9da2ee815dbe14d8b5dcda67661096cb6c31
Max Schmitt <max@schmitt.mx>
2021-04-21 11:15:13 +0200

    hore: move Thumbnail generation to chrome-aws-lambda

--- src/pages/api/thumbnail.ts
+++ src/pages/api/thumbnail.ts
@@ -1,26 +1,35 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-import * as playwright from 'playwright-aws-lambda';
+import playwright from 'playwright-core';
+import chromium from 'chrome-aws-lambda';
 import { getAbsoluteURL } from 'utils/utils';
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  await playwright.loadFont("https://raw.githack.com/googlei18n/noto-emoji/master/fonts/NotoColorEmoji.ttf")
+  try {
+    await chromium.font("https://raw.githack.com/googlei18n/noto-emoji/master/fonts/NotoColorEmoji.ttf")
+    const browser = await playwright.chromium.launch({
+      args: chromium.args,
+      executablePath: await chromium.executablePath || '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
+      headless: chromium.headless,
+    });
 
-  const browser = await playwright.launchChromium();
-  const page = await browser.newPage({
-    viewport: {
-      width: 1200,
-      height: 630
-    }
-  });
-  const url = getAbsoluteURL(req.query["path"] as string || "")
-  await page.goto(url, {
-    timeout: 15 * 1000
-  })
-  const data = await page.screenshot({
-    type: "png"
-  })
-  await browser.close()
-  res.setHeader("Cache-Control", "s-maxage=31536000, stale-while-revalidate")
-  res.setHeader('Content-Type', 'image/png')
-  res.end(data)
+    const page = await browser.newPage({
+      viewport: {
+        width: 1200,
+        height: 630
+      }
+    });
+    const url = getAbsoluteURL(req.query["path"] as string || "")
+    await page.goto(url, {
+      timeout: 15 * 1000
+    })
+    const data = await page.screenshot({
+      type: "png"
+    })
+    await browser.close()
+    res.setHeader("Cache-Control", "s-maxage=31536000, stale-while-revalidate")
+    res.setHeader('Content-Type', 'image/png')
+    res.end(data)
+  } catch (err) {
+    console.error(err)
+  }
 }
